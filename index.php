ff30b20bef22798d77f79489602c42f181daf28b
Ferdynda <116608710+Ferdynda@users.noreply.github.com>
2022-10-25 13:33:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -99,5 +99,24 @@
                 </div>
         </div>
     </section>
+     <section class="bg-gray-200 h-full py-5">
+        <div class="container mx-auto"></div>
+        <div class="w-1/2 mx-auto">
+            <h2 class="text-left text-2xl font-semibold pt-7 pl-7 mb-2">Lokasi Toko</h2>
+            <div class="flex justify-center mb-12 gap-3">
+                <div class="m-2/4">
+                    <iframe class="shadow-lg rounded-xl 2/4" width="100%" height="250px" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15812.72377284849!2d110.3605617!3d-7.7706286!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe3901851e95b2001!2sDomaiNesia!5e0!3m2!1sid!2sid!4v1664152754784!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="m-2/4">
+                    <h3 class="font-bold">Head Office</h3>
+                    <p class="my-2">Jl. Kopo Katapang KM 12.8 Bandung 40971 - Indonesia</p>
+                    <p class="my-2"><strong>Phone</strong> : +62-22 58991445</p>
+                    <p class="my-2"><strong>Fax</strong> : +62-22 58991446</p>
+                    <p class="mt-2"><strong>WhatsApp</strong> : +62-22 58991446</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 </body>
 </html>
